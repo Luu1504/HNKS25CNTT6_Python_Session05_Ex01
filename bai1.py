b936101b96ca2b87848c2bcdf9665e761f7a6578
sShugen <hotsuper75@gmail.com>
2026-05-21 19:37:45 +0700

    Delete bai1.py

--- bai1.py
+++
@@ -1,9 +0,0 @@
-initial_price = int(input("Nhập số tiền ban đầu: "))
-
-print("--- HÓA ĐƠN THANH TOÁN RIKKEI STORE ---")
-
-if initial_price > 500000:
-    total_price = initial_price * 0.1
-else:
-    total_price = initial_price
-print(f"Tổng số tiền khách phải trả là: {total_price}")
